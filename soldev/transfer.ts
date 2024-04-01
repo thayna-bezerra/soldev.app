7b60f5453bce204cd6c0256cd8512c3636002fbf
thayna <arraisthayna2106@gmail.com>
2024-03-31 23:27:40 -0300

    completes the transaction and sends

--- soldev/transfer.ts
+++ soldev/transfer.ts
@@ -26,3 +26,25 @@ import {
   console.log(
     `✅ Loaded our own keypair, the destination public key, and connected to Solana`
   );
+
+  
+  const transaction = new Transaction();
+  
+  const LAMPORTS_TO_SEND = 5000;
+  
+  const sendSolInstruction = SystemProgram.transfer({
+    fromPubkey: senderKeypair.publicKey,
+    toPubkey,
+    lamports: LAMPORTS_TO_SEND,
+  });
+  
+  transaction.add(sendSolInstruction);
+  
+  const signature = await sendAndConfirmTransaction(connection, transaction, [
+    senderKeypair,
+  ]);
+  
+  console.log(
+    `💸 Finished! Sent ${LAMPORTS_TO_SEND} to the address ${toPubkey}. `
+  );
+  console.log(`Transaction signature is ${signature}!`);
