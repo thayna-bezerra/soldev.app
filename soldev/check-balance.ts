869bcce0803eaeba35ec1bb01c1aac721a42415e
thayna <arraisthayna2106@gmail.com>
2024-03-31 23:09:31 -0300

    check the balance of any wallet

---
+++ soldev/check-balance.ts
@@ -0,0 +1,18 @@
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+
+const suppliedPublicKey = process.argv[2];
+if (!suppliedPublicKey) {
+  throw new Error("Provide a public key to check the balance of!");
+}
+
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
+const publicKey = new PublicKey(suppliedPublicKey);
+
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+  `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+);
